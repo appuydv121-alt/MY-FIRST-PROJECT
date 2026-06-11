cffa051859106fe5d4fb9b240bfcff184163e9e9
Arpit Yadav <appuydv121@gmail.com>
2026-06-11 08:33:04 +0530

    firstpage animation done

--- script.js
+++ script.js
@@ -9,4 +9,29 @@ function circleMouseFollower(){
         document.querySelector('#minicircle').style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
     })
 }
+
+function pageTransition(){
+    var tl = gsap.timeline();
+    tl.from('#nav' , {
+        y:"-10",
+        opacity: 0,
+        duration: 1,
+        ease:Expo.easeInOut
+    })
+    .to('.boundingElement' , {
+        y:0,
+        ease:Expo.easeInOut,
+        duration: 2,
+        stagger:0.2,
+        delay: -1
+    })
+        .from('#herofooter' , {
+            y: "-10",
+            opacity: 0,
+            duration: 1.5,
+            ease: Expo.easeInOut,
+            delay: -1
+        });
+}
 circleMouseFollower();
+pageTransition();
