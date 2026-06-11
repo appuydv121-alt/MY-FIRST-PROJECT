6fbc23be2102b247576243e444f8c9fcec9a53f9
Arpit Yadav <appuydv121@gmail.com>
2026-06-11 08:03:08 +0530

    mouse tracker created with a issue of scrolling

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true
 });
+
+function circleMouseFollower(){
+    window.addEventListener('mousemove', function(dets){
+        // console.log(dets.clientX, dets.clientY);
+        document.querySelector('#minicircle').style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+    })
+}
+circleMouseFollower();
